4da3a97b38e95072e428c550fe9c76217a7008f2
Xing Liu <starrr826@gmail.com>
2026-01-15 20:17:59 +0900

    refactoring done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts "Creating 5 random restaurants..."
+5.times do |i|
+
+  category = categories.sample
+
+  Restaurant.create!(
+    name: "#{category.capitalize} Authentic Cuisine",
+    address: "#{category.capitalize} Street, No. #{rand(1..10)}",
+    phone_number: "09#{rand(10000000..99999999)}",
+    category: category
+  )
+end
+
+puts "Finished!"
